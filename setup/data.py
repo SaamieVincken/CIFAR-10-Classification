d1951413bc10c7e538c9bcba75b0dd2ffeb130e9
saamievincken <saamievincken@protonmail.com>
2024-09-18 17:14:16 +0200

    Update data augmentation

--- setup/data.py
+++ setup/data.py
@@ -2,15 +2,28 @@ import torch
 import torchvision
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2 as transforms
+from torchvision.transforms.v2 import RandomCrop
 
 
-# Convert to PyTorch tensor and normalize between [-1, 1]
-def get_transform():
-    return transforms.Compose([
-        transforms.ToImage(),
-        transforms.ToDtype(torch.float32, scale=True),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+# Convert to PyTorch tensor and normalize between [-1, 1], optional data augmentation
+def get_transform(augment=False):
+    if augment:
+        return transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            RandomCrop(32, padding=4),
+            # transforms.RandomCrop(32, padding=4),
+            # transforms.ColorJitter(brightness=1.0, contrast=0.5, saturation=1, hue=0.1),
+            # transforms.RandomRotation(10),
+            transforms.ToImage(),
+            transforms.ToDtype(torch.float32, scale=True),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    else:
+        return transforms.Compose([
+            transforms.ToImage(),
+            transforms.ToDtype(torch.float32, scale=True),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
 
 # Download and prepare CIFAR-10 dataset with given transformation
