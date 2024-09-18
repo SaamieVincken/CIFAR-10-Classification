b91b22600f171a00fe987c8da13963d8021d5eca
saamievincken <saamievincken@protonmail.com>
2024-09-18 17:14:35 +0200

    Update train function

--- train.py
+++ train.py
@@ -1,51 +1,66 @@
 import torch
 import wandb
 import torchmetrics
-from setup.config import get_metrics_config
+from setup.config import get_metrics_config, get_device
 
 # Initialize metrics
-accuracy_metric, precision_metric, recall_metric, f1_metric = get_metrics_config()
+accuracy_metric, precision_metric, recall_metric, f1_metric = get_metrics_config(get_device())
 
 
-# Training function
-def train_model(model, trainloader, optimizer, criterion, device='cpu'):
-
+def train_epoch(epoch, model, trainloader, optimizer, criterion, device='cpu', scheduler=None, L1=None):
     model.train()
+    running_loss = 0.0
+    accuracy_metric.reset()
+    precision_metric.reset()
+    recall_metric.reset()
+    f1_metric.reset()
 
-    # Temp: This code is only used for the first part of the assignment
-    # Get a single batch of data (images and labels)
-    data_iter = iter(trainloader)
-    images, labels = next(data_iter)
+    for images, labels in trainloader:
+        images, labels = images.to(device), labels.to(device)
+        outputs, loss = process_batch(optimizer, device, model, criterion, images, labels, L1)
+        update_metrics(outputs, labels)
+        running_loss += loss.item()
 
-    # Move data to device
-    images, labels = images.to(device), labels.to(device)
-
-    # Reset gradients
-    optimizer.zero_grad()
-
-    # Get model output
-    outputs = model(images)
-
-    # Get the loss
-    loss = criterion(outputs, labels)
-
-    # Calculate the gradient
-    loss.backward()
-
-    # Update learning weights
-    optimizer.step()
-
-    # Calculate metrics
-    accuracy = accuracy_metric(outputs, labels)
-    precision = precision_metric(outputs, labels)
-    recall = recall_metric(outputs, labels)
-    f1_score = f1_metric(outputs, labels)
+    # Calculate metrics per epoch
+    epoch_loss = running_loss / len(trainloader)
+    epoch_accuracy = accuracy_metric.compute().item()
+    epoch_precision = precision_metric.compute().item()
+    epoch_recall = recall_metric.compute().item()
+    epoch_f1 = f1_metric.compute().item()
 
     # Log metrics to W&B
     wandb.log({
-        'loss': loss.item(),
-        'accuracy': accuracy,
-        'precision': precision,
-        'recall': recall,
-        'f1_score': f1_score
+        'epoch': epoch + 1,
+        'training_loss': epoch_loss,
+        'training_accuracy': epoch_accuracy,
+        'training_precision': epoch_precision,
+        'training_recall': epoch_recall,
+        'training_f1_score': epoch_f1,
     })
+
+    scheduler.step()
+
+    return epoch_loss, epoch_accuracy, epoch_precision, epoch_recall, epoch_f1
+
+
+def process_batch(optimizer, device, model, criterion, images, labels, L1=None):
+    images, labels = images.to(device), labels.to(device)
+    optimizer.zero_grad()
+    outputs = model(images)
+    loss = criterion(outputs, labels)
+
+    if L1:
+        # Add L1 regularization
+        L1_loss = sum(p.abs().sum() for p in model.parameters())
+        loss += L1 * L1_loss
+
+    loss.backward()
+    optimizer.step()
+    return outputs, loss
+
+
+def update_metrics(outputs, labels):
+    accuracy_metric.update(outputs, labels)
+    precision_metric.update(outputs, labels)
+    recall_metric.update(outputs, labels)
+    f1_metric.update(outputs, labels)
